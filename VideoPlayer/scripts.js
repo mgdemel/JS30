9f6b07f9d0932808d41ae24aaef82b629831ab3a
Melissa <melissa.alasalmi@gmail.com>
2022-02-04 15:23:24 +0200

    added progress bar updating function + skipping time function

--- VideoPlayer/scripts.js
+++ VideoPlayer/scripts.js
@@ -1,12 +1,28 @@
-// Build functions
 // - Volume slider
 // - Speed of play slider
-// - Skip forward/backward buttons
 
+// const app = document.querySelector(".player");
 const player = document.querySelector(".viewer");
+const progressBar = document.querySelector(".progress__filled")
 const playButton = document.querySelector(".toggle");
 const volumeSlider = document.querySelector("input[name='volume']");
 const speedSlider = document.querySelector("input[name='playbackRate']");
+const skippers = document.querySelectorAll("button[data-skip]");
+
+function updateProgress() {
+  console.log('progress updated');
+  let playPercentage = (player.currentTime * 100) / player.duration;
+  progressBar.style.flexBasis = `${playPercentage}%`;
+}
+
+function handleSkip() {
+  player.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleSpeed() {
+  console.log(speedSlider);
+  console.dir(speedSlider);
+}
 
 function handleSound() {
   console.log(volumeSlider);
@@ -23,6 +39,9 @@ function playPause() {
   }
 }
 
+player.addEventListener("timeupdate", updateProgress);
 player.addEventListener("click", playPause);
 playButton.addEventListener("click", playPause);
 volumeSlider.addEventListener("click", handleSound);
+speedSlider.addEventListener("click", handleSpeed);
+skippers.forEach((skipper) => skipper.addEventListener("click", handleSkip));
